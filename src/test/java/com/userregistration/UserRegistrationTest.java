702d952d80feefaa46fa33be4b260e76f1bac80c
jitesh456 <jitesh0dubey@gmail.com>
2020-02-17 18:54:31 +0530

    ADDED:Validation for last name

--- src/test/java/com/userregistration/UserRegistrationTest.java
+++ src/test/java/com/userregistration/UserRegistrationTest.java
@@ -8,19 +8,26 @@ public class UserRegistrationTest{
     @Test
     public void givenFirstName_WhenProper_ReturnTrue() {
 
-        Boolean result = registration.validateName("Jitesh");
+        Boolean result = registration.validateName("Rohit");
         Assert.assertEquals(true,result);
     }
 
     @Test
     public void givenFirstName_WhenNotProper_ReturnFalse() {
 
-        Boolean result= registration.validateName("ji");
+        Boolean result= registration.validateName("ro");
         Assert.assertEquals(false,result);
     }
 
     @Test
     public void lastName_WhenProper_ReturnTrue() {
-        Boolean result =registration.validateName();
+        Boolean result =registration.validateName("Sharma");
+        Assert.assertEquals(true,result);
+    }
+
+    @Test
+    public void lastName_WhenNotProper_ReturnFalse() {
+        Boolean result = registration.validateName("sh1");
+        Assert.assertEquals(false,result);
     }
 }
