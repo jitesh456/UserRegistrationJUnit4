23939091934dd9d92194b19db99e9b6fdc3d2976
jitesh456 <jitesh0dubey@gmail.com>
2020-02-18 12:24:14 +0530

    ADDED:Password validation for upper case

--- src/main/java/com/userregistration/UserRegistration.java
+++ src/main/java/com/userregistration/UserRegistration.java
@@ -5,6 +5,7 @@ public class UserRegistration {
     String emailValidate= "^[a-zA-Z]{3,}([-|+|.|_]?[a-zA-Z0-9]+)?[@]{1}[A-Za-z0-9]+[.]{1}[a-zA-Z]{2,4}([.]{1}[a-zA-Z]+)?$";
     String validateNo="^[0-9]{1,3}[ ][0-9]{10}$";
     String validatePassword1="^[a-zA-Z]{8,}";
+    String validatePassword2="((?=.*[A-Z])(?=.*[a-z])).{8,}";
     public Boolean validateName(String name) {
         return name.matches(nameValidation);
     }
@@ -20,4 +21,10 @@ public class UserRegistration {
     public Boolean validatePassword(String password) {
         return password.matches(validatePassword1);
     }
+
+    public Boolean validatePasswordForUpper(String password) {
+        return password.matches(validatePassword2);
+    }
+
+
 }
