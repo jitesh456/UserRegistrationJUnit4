2fec1bc54da9efe37c7a4bf05e22466ea8ef8cd1
jitesh456 <jitesh0dubey@gmail.com>
2020-02-18 10:17:29 +0530

    ADDED:Validation for Email id

--- src/main/java/com/userregistration/UserRegistration.java
+++ src/main/java/com/userregistration/UserRegistration.java
@@ -1,8 +1,14 @@
 package com.userregistration;
 
 public class UserRegistration {
-    String nameValidation="^[A-Z]{1}[a-z]{2,}";
+    String VALIDATION_NAME = "^[A-Z]{1}[a-z]{2,}";
+    String VALIDATION_EMAIL = "^[a-zA-Z]{3,}([-|+|.|_]?[a-zA-Z0-9]+)?[@]{1}[A-Za-z0-9]+[.]{1}[a-zA-Z]{2,4}([.]{1}[a-zA-Z]+)?$";
+
     public Boolean validateName(String name) {
-        return name.matches(nameValidation);
+        return name.matches(VALIDATION_NAME);
+    }
+
+    public Boolean validateEmail(String email) {
+        return email.matches(VALIDATION_EMAIL);
     }
 }
