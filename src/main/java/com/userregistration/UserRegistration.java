2d0b822bd43df29bccbfe11e77c3dcab5bdf2995
jitesh456 <jitesh0dubey@gmail.com>
2020-02-18 16:28:39 +0530

    ADDED:Validation For all sample email

--- src/main/java/com/userregistration/UserRegistration.java
+++ src/main/java/com/userregistration/UserRegistration.java
@@ -1,39 +1,40 @@
 package com.userregistration;
 
 public class UserRegistration {
-    String nameValidation="^[A-Z]{1}[a-z]{2,}";
-    String emailValidate= "^[a-zA-Z]{3,}([-|+|.|_]?[a-zA-Z0-9]+)?[@]{1}[A-Za-z0-9]+[.]{1}[a-zA-Z]{2,4}([.]{1}[a-zA-Z]+)?$";
-    String validateNo="^[0-9]{1,3}[ ][0-9]{10}$";
-    String validatePasswordMinmumCharacter="^[a-zA-Z]{8,}";
-    String validatePasswordUpperCase="((?=.*[A-Z])(?=.*[a-z])).{8,}";
-    String validatePasswordDigit="((?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])).{8,}";
-    String validatePasswordSpecial="((?=.*[A-Z])(?=.*[a-z])(?=.*[@#$!%].)(?=.*[0-9])).{8,}";
+    String VALIDATION_NAME = "^[A-Z]{1}[a-z]{2,}$";
+    String VALIDATION_EMAIL = "^[a-zA-Z]{3,}([-|+|.|_]?[a-zA-Z0-9]+)?[@]{1}[A-Za-z0-9]+[.]{1}[a-zA-Z]{2,4}([.]{1}[a-zA-Z]+)?$";
+    String VALIDATE_PHONE_NUMBER = "^[0-9]{1,3}[ ][0-9]{10}$";
+    String VALIDATE_PASSWORD_MINIMUM_CHARACTER = "^[a-zA-Z]{8,}$";
+    String VALIDATE_PASSWORD_UPPER_CASE = "^(?=.*[A-Z])[a-zA-Z]{8,}$";
+    String VALIDATE_PASSWORD_DIGIT = "^((?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])).{8,}$";
+    String VALIDATE_PASSWORD_SPECIAL_CHARACTER = "^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=[^$@!#%*?&]*[$#@!%*?&][^$@!#%*?&]*$).{8,}";
+
     public Boolean validateName(String name) {
-        return name.matches(nameValidation);
+        return name.matches(VALIDATION_NAME);
     }
 
     public Boolean validateEmail(String email) {
-        return email.matches(emailValidate);
+        return email.matches(VALIDATION_EMAIL);
     }
 
     public Boolean validatePhoneNo(String phoneNo) {
-        return phoneNo.matches(validateNo);
+        return phoneNo.matches(VALIDATE_PHONE_NUMBER);
     }
 
     public Boolean validatePassword(String password) {
-        return password.matches(validatePasswordMinmumCharacter);
+        return password.matches(VALIDATE_PASSWORD_MINIMUM_CHARACTER);
     }
 
     public Boolean validatePasswordForUpper(String password) {
-        return password.matches(validatePasswordUpperCase);
+        return password.matches(VALIDATE_PASSWORD_UPPER_CASE);
     }
 
 
     public Boolean validatePasswordForNumber(String password) {
-        return password.matches(validatePasswordDigit);
+        return password.matches(VALIDATE_PASSWORD_DIGIT);
     }
 
     public Boolean validatePasswordSpecialCharacter(String password) {
-        return password.matches(validatePasswordSpecial);
+        return password.matches(VALIDATE_PASSWORD_SPECIAL_CHARACTER);
     }
 }
